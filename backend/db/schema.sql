9b3197490badffafec8a5105f7cf7462201813fa
Sung Yi <dreamseekerfromn@gmail.com>
2025-02-27 03:31:26 -0500

    working on the basic schema

---
+++ backend/db/schema.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE mountains_vs_beaches(
+    id SERIAL PRIMARY KEY,
+    Age INT,
+    Gender VARCHAR(10),
+    Income INT,
+    Education_Level VARCHAR(20),
+    Travel_Frequency INT,
+    Preferred_Activities VARCHAR(20),
+    Vacation_Budget INT,
+    Location VARCHAR(20),
+    Proximity_to_Mountains INT,
+    Proximity_to_Beaches INT,
+    Favorite_Season VARCHAR(6),
+    Pets INT,
+    Environmental_Concerns INT,
+    Preference INT
+)
